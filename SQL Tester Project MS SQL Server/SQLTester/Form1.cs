f1500609c649b97887037d0b4d5be330f379cf35
Lane <lanec92@gmail.com>
2021-02-09 22:07:30 -0600

    Finished Project

---
+++ SQL Tester Project MS SQL Server/SQLTester/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLTester
+{
+    public partial class frmTester : Form
+    {
+        SqlConnection conn;
+        public frmTester()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTester_Load(object sender, EventArgs e)
+        {
+            var connString = "Data Source=ALIEN;Initial Catalog=Books;Integrated Security=True";
+            conn = new SqlConnection(connString);
+            conn.Open();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = null;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            try
+            {
+                cmd = new SqlCommand(txtCommand.Text, conn);
+                adapter.SelectCommand = cmd;
+                adapter.Fill(table);
+
+                grdRecords.DataSource = table;
+                lblCount.Text = table.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error In SQL Command", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            cmd.Dispose();
+            adapter.Dispose();
+            table.Dispose();
+        }
+
+        private void frmFormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
